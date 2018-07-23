28b83a9970e5fb3fecb1e797ee3cc8b73f2972f0
sathya narayanan <sathyanarayanan@sathyas-MacBook-Pro.local>
2018-07-23 13:11:54 +0530

    first commit

---
+++ js/menu.js
@@ -0,0 +1,52 @@
+/* view profile */
+$(".pic").click(function() {
+  $('.profile').css('left', '0');
+  $('.main').css('left', '-100%');
+});
+$(".back_main").click(function() {
+  $('.profile').css('left', '-100%');
+  $('.main').css('left', '0');
+});
+
+/* sending msg in chat */
+$(document).on('keyup', '.reply_main input', function() {
+  if ($(this).val().length > 0) {
+    $('.reply_recording').hide();
+    $('.reply_send').show();
+  } else {
+    $('.reply_send').hide();
+    $('.reply_recording').show();
+  }
+});
+
+/* staring hide item */
+$('.reply_send').hide();
+$('.back_chat').hide();
+
+/* handle menu list click */
+$(".menu_row").click(function() {
+  isclicked = true;
+  if ($(window).width() <= 767) {
+    $('.main_side').hide();
+    $('.chat_side').show();
+    $('.back_chat').show();
+  }
+});
+
+/* back button on chat page */
+$(".back_chat").click(function() {
+  $('.main_side').show();
+  $('.chat_side').hide();
+});
+
+/* handel resize window */
+$(window).on('resize', function() {
+  var win = $(this);
+  if ($(window).width() <= 767) {
+    $('.main_side').show();
+    $('.chat_side').hide();
+  } else {
+    $('.main_side').show();
+    $('.chat_side').show();
+  }
+});
